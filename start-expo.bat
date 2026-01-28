ac167c77f8313e12bdacdedd141d425350120369
PKLITB26 <pklitb26@gmail.com>
2026-01-28 09:40:12 +0700

    Pembersihan file yang tidak digunakan untuk merapikan struktur proyek

--- start-expo.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Membersihkan cache Expo...
-npx expo start --clear --lan
-
-echo.
-echo Jika masih error, coba:
-echo 1. Pastikan server PHP berjalan di http://10.251.109.65/hadirinapp
-echo 2. Restart Expo Go app
-echo 3. Scan QR code lagi
